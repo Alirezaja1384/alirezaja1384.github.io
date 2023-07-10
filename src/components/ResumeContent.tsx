eb02fcac221983d612fc7b9500b5d6b6fa40840f
Alireza Jafari <alirezaja1384@gmail.com>
2023-07-10 23:25:23 +0330

    Work experience implemented

--- src/components/ResumeContent.tsx
+++ src/components/ResumeContent.tsx
@@ -1,8 +1,10 @@
+import EngineeringIcon from "@mui/icons-material/Engineering";
 import BuildCircleIcon from "@mui/icons-material/BuildCircle";
 import LightbulbCircleIcon from "@mui/icons-material/LightbulbCircle";
 
 import SpecialThanksComponent from "./SpecialThanks";
 import PercentageRepresentationComponent from "./PercentageRepresentation";
+import WorkExperiencesComponent, { WorkExperience } from "./WorkExperiences";
 
 const skills = [
     { name: "Python", percentage: 70 },
@@ -27,6 +29,33 @@ const interests = [
     { name: "SQL", percentage: 50 },
 ];
 
+const workExperiences: WorkExperience[] = [
+    {
+        title: "پروژه مجاز آموز",
+        description: "پروژه تمرینی جهت تسلط به Django",
+        companyName: null,
+        companyWebsite: null,
+        startDate: "2021-03-26",
+        endDate: "2021-10-30",
+    },
+    {
+        title: "کارآموز React",
+        description: "یادگیری React و پیاده سازی چند پروژه تمرینی",
+        companyName: "شتابدهنده پیشگامان",
+        companyWebsite: "https://shetabdehi.com/",
+        startDate: "2021-10-23",
+        endDate: "2022-03-20",
+    },
+    {
+        title: "برنامه نویس Django و React",
+        description: "پیاده سازی Backend پروژه اپ سرام و Frontend پروژه نماد",
+        companyName: "شتابدهنده پیشگامان",
+        companyWebsite: "https://shetabdehi.com/",
+        startDate: "2022-03-21",
+        endDate: "2022-09-22",
+    },
+];
+
 function ResumeContentComponent() {
     return (
         <div className="flex flex-col flex-grow p-5">
@@ -35,11 +64,18 @@ function ResumeContentComponent() {
                 icon={<LightbulbCircleIcon fontSize="large" />}
                 interests={interests}
             />
+
             <PercentageRepresentationComponent
                 title="توانایی ها"
                 icon={<BuildCircleIcon fontSize="large" />}
                 interests={skills}
             />
+
+            <WorkExperiencesComponent
+                icon={<EngineeringIcon fontSize="large" />}
+                workExperiences={workExperiences}
+            />
+
             <SpecialThanksComponent />
         </div>
     );
--- src/components/WorkExperiences.tsx
+++ src/components/WorkExperiences.tsx
@@ -1,5 +1,66 @@
-function WorkExperiencesComponent() {
-    return <></>;
+import React from "react";
+import { toLocalDate } from "../utils/date";
+
+export interface WorkExperience {
+    title: string;
+    description: string;
+    companyName: string | null;
+    companyWebsite: string | null;
+    startDate: string;
+    endDate: string;
+}
+
+function WorkExperiencesComponent({
+    icon,
+    workExperiences,
+}: {
+    icon?: React.ReactElement;
+    workExperiences: WorkExperience[];
+}) {
+    return (
+        <div className="m-2">
+            <h2 className="font-bold text-xl text-blue">{icon} تجربه کاری:</h2>
+            <div>
+                {workExperiences.map((experience) => (
+                    <div className="my-3">
+                        <div className="flex flex-wrap justify-between">
+                            <h4 className="font-bold text-lg">
+                                {experience.title}
+                            </h4>
+                            {experience.startDate && experience.endDate && (
+                                <span className="text-gray-600">
+                                    از{" "}
+                                    {toLocalDate(experience.startDate, "short")}{" "}
+                                    تا{" "}
+                                    {toLocalDate(experience.endDate, "short")}
+                                </span>
+                            )}
+                        </div>
+                        {experience.companyName && (
+                            <p className="text-gray-600">
+                                {experience.companyName}
+                                {experience.companyWebsite && (
+                                    <>
+                                        {" "}
+                                        |{" "}
+                                        <a
+                                            className="no-style"
+                                            href={experience.companyWebsite}
+                                        >
+                                            وبسایت شرکت
+                                        </a>
+                                    </>
+                                )}
+                            </p>
+                        )}
+                        <p className="text-gray-600">
+                            {experience.description}
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default WorkExperiencesComponent;
