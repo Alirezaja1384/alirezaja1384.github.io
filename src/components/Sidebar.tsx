ebf2cfc017645218a7cfac96b88184daa61cd828
Alireza Jafari <alirezaja1384@gmail.com>
2023-07-11 01:37:22 +0330

    SidebarComponent: Minor visual improvement

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,18 +10,20 @@ function SidebarComponent() {
                 jobTitle="توسعه دهنده وب"
                 imageUrl={null}
             />
-            <MoreAboutMeComponent
-                age={18}
-                birthDate="2005-08-16"
-                jobStatus={JobStatus.LOOKING_FOR_JOB}
-            />
-            <ContactMeComponent
-                cellphoneNumber="+989386288279"
-                emailAddress="alirezaja1384@gmail.com"
-                linkedinUsername="alirezaja1384"
-                telegramUsername="alirezaja1384"
-                githubUsername="alirezaja1384"
-            />
+            <div className="py-5 lg:py-2">
+                <MoreAboutMeComponent
+                    age={18}
+                    birthDate="2005-08-16"
+                    jobStatus={JobStatus.LOOKING_FOR_JOB}
+                />
+                <ContactMeComponent
+                    cellphoneNumber="+989386288279"
+                    emailAddress="alirezaja1384@gmail.com"
+                    linkedinUsername="alirezaja1384"
+                    telegramUsername="alirezaja1384"
+                    githubUsername="alirezaja1384"
+                />
+            </div>
         </div>
     );
 }
