4eabc7331e814b40f0cfdd8769e9a74bf80b8f9b
Alireza Jafari <alirezaja1384@gmail.com>
2023-07-12 23:13:25 +0330

    Responsive design improvement

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -4,14 +4,14 @@ import ResumeSidebarComponent from "./ResumeSidebar";
 function ResumeComponent({ resume }: { resume: Resume }) {
     return (
         <>
-            <div className="flex w-full md:w-[35%] lg:w-[30%]">
+            <div className="flex w-full md:w-auto">
                 <ResumeSidebarComponent
                     about={resume.about}
                     contactInfo={resume.contactInfo}
                 />
             </div>
 
-            <div className="flex w-full md:w-[65%] lg:w-[70%]">
+            <div className="flex flex-grow">
                 <ResumeContentComponent
                     interests={resume.interests}
                     skills={resume.skills}
