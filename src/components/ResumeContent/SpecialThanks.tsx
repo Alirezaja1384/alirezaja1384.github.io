48b186ea209244e02ebb76011368e0c660256fc9
Alireza Jafari <alirezaja1384@gmail.com>
2023-07-31 00:45:36 +0330

    SpecialThanks.tsx: FreeDNS

--- src/components/ResumeContent/SpecialThanks.tsx
+++ src/components/ResumeContent/SpecialThanks.tsx
@@ -2,6 +2,7 @@ import AppsIcon from "@mui/icons-material/Apps";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import HourglassBottomIcon from "@mui/icons-material/HourglassBottom";
 import ArticleIcon from "@mui/icons-material/Article";
+import LanguageIcon from "@mui/icons-material/Language";
 
 function SpecialThanksComponent() {
     return (
@@ -24,8 +25,7 @@ function SpecialThanksComponent() {
                             rel="noreferrer"
                             href="https://www.flaticon.com/authors/smashicons"
                         >
-                            <AppsIcon /> آیکون کاربر از توسط Smashicons -
-                            Flaticon
+                            <AppsIcon /> آیکون کاربر از Smashicons - Flaticon
                         </a>
                     </li>
                     <li>
@@ -44,6 +44,15 @@ function SpecialThanksComponent() {
                             href="https://icons8.com/icon/115648/resume"
                         >
                             <ArticleIcon /> آیکون رزومه از Icons8
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            target="_blank"
+                            rel="noreferrer"
+                            href="https://freedns.afraid.org/"
+                        >
+                            <LanguageIcon /> پروژه‌ی FreeDNS
                         </a>
                     </li>
                 </ul>
