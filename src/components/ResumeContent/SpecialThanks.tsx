f7e36fb1cf24c43eebbbb9d83b83a7586cddfc65
Alireza Jafari <alirezaja1384@gmail.com>
2023-07-31 00:55:53 +0330

    SpecialThanks.tsx: Minor improvements

--- src/components/ResumeContent/SpecialThanks.tsx
+++ src/components/ResumeContent/SpecialThanks.tsx
@@ -1,8 +1,47 @@
+import React from "react";
 import AppsIcon from "@mui/icons-material/Apps";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import HourglassBottomIcon from "@mui/icons-material/HourglassBottom";
 import ArticleIcon from "@mui/icons-material/Article";
 import LanguageIcon from "@mui/icons-material/Language";
+
+const specialThanks: {
+    id: string;
+    href: string;
+    text: string;
+    icon: React.ReactElement;
+}[] = [
+    {
+        id: "online-cv",
+        text: "پروژه‌ی online-cv از sharu725",
+        icon: <GitHubIcon />,
+        href: "https://github.com/sharu725/online-cv",
+    },
+    {
+        id: "free-dns",
+        text: "پروژه‌ی FreeDNS از Joshua Anderson",
+        icon: <LanguageIcon />,
+        href: "https://freedns.afraid.org/",
+    },
+    {
+        id: "user-icon",
+        text: "آیکون کاربر از Smashicons - Flaticon",
+        icon: <AppsIcon />,
+        href: "https://www.flaticon.com/authors/smashicons",
+    },
+    {
+        id: "loading-icon",
+        text: "آیکون بارگذاری از loading.io",
+        icon: <HourglassBottomIcon />,
+        href: "https://loading.io/",
+    },
+    {
+        id: "resume-icon",
+        text: "آیکون رزومه از Icons8",
+        icon: <ArticleIcon />,
+        href: "https://icons8.com/icon/115648/resume",
+    },
+];
 
 function SpecialThanksComponent() {
     return (
@@ -10,51 +49,13 @@ function SpecialThanksComponent() {
             <h2 className="font-bold text-xl">تشکر ویژه:</h2>
             <div className="mr-1">
                 <ul>
-                    <li>
-                        <a
-                            target="_blank"
-                            rel="noreferrer"
-                            href="https://github.com/sharu725/online-cv"
-                        >
-                            <GitHubIcon /> پروژه‌ی online-cv از sharu725
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            target="_blank"
-                            rel="noreferrer"
-                            href="https://www.flaticon.com/authors/smashicons"
-                        >
-                            <AppsIcon /> آیکون کاربر از Smashicons - Flaticon
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            target="_blank"
-                            rel="noreferrer"
-                            href="https://loading.io/"
-                        >
-                            <HourglassBottomIcon /> آیکون بارگذاری از loading.io
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            target="_blank"
-                            rel="noreferrer"
-                            href="https://icons8.com/icon/115648/resume"
-                        >
-                            <ArticleIcon /> آیکون رزومه از Icons8
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            target="_blank"
-                            rel="noreferrer"
-                            href="https://freedns.afraid.org/"
-                        >
-                            <LanguageIcon /> پروژه‌ی FreeDNS
-                        </a>
-                    </li>
+                    {specialThanks.map((st) => (
+                        <li key={st.id}>
+                            <a target="_blank" rel="noreferrer" href={st.href}>
+                                {st.icon} {st.text}
+                            </a>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
